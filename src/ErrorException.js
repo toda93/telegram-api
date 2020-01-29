ecbe5a88bc463304b6accd99e9386b577d9d9856
toda93 <dttong93@gmail.com>
2020-01-29 17:57:01 +0700

    sync

--- src/ErrorException.js
+++
@@ -1,8 +0,0 @@
-export class ErrorException {
-    constructor(code, errors = []) {
-        this.name = 'ErrorException';
-        this.message = code;
-        this.code = code;
-        this.errors = errors;
-    }
-}
